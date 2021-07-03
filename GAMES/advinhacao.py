9316c1ca6c06e5e1042bb5db5c44f30d39c7945a
Camilli Cerutti <85898218+CamilliCerutti@users.noreply.github.com>
2021-07-03 12:01:37 -0300

    editando

--- GAMES/advinhacao.py
+++ GAMES/advinhacao.py
@@ -1,18 +1,59 @@
+# Jogo de adivinhação
 import requests
 import random
-# entender como funciona as requests
-# fazer parte que pergunta ao usuario uma letra
-# verificar se foi digitado apenas uma letra
-# colocar as letras que ja foram digitadas numa lista
-# imprimir um * nas letras que aind anão foram digitadas
-# fazer uma contagem de chances
 
-url ='https://www.ime.usp.br/~pf/dicios/br-utf8.txt'
+# palavra = 'perfume'
+digitadas = []
+chances = 5
+
+url = 'https://www.ime.usp.br/~pf/dicios/br-utf8.txt'
 
 resposta = requests.get(url)
-palavras = resposta.content.splitlines()
 
-palavras = [ palavra.decode('utf-8')for palavra in palavras]
+u = resposta.content.splitlines()
 
-palavras_aleatorias = random.sample(palavras, 10)
+u = [palavra.decode('utf-8') for palavra in u]
 
+palavras = random.sample(u, 1)
+
+while True:
+    # if chances <= 0:
+    #     print('suas chances acabaram, voce perdeu!')
+    #     break
+
+    letra = input('Digite uma letra: ')
+
+    if len(letra) > 1:
+        print('Ahhh isso não vale, digite apenas uma letra.')
+        continue
+
+    digitadas.append(letra)
+
+    if letra in palavras:
+        print(f'A letra {letra} existe na palavra secreta')
+
+    else:
+        print(f' A letra {letra} Nao existe na palavra secreta')
+        digitadas.pop()
+
+    temp = ''
+
+    for letra_secreta in palavras:
+        if letra_secreta in digitadas:
+            temp += letra_secreta
+
+        else:
+            temp += '*'
+
+    if temp == palavras:
+        print(f'Que legal, Voce ganhou! A palavra é: {temp}')
+        break
+    else:
+        print(f'A palavra secreta esta assim: {temp}')
+
+    if letra not in palavras:
+        chances -= 1
+
+    print(f'Voce ainda tem {chances} chances.')
+
+    print(palavras)
