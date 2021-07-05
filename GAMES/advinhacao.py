cc26850941cbcacbff439a4337c151ab1a89d4f7
Camilli Cerutti <85898218+CamilliCerutti@users.noreply.github.com>
2021-07-05 15:38:25 -0300

    resolvi o jogo

--- GAMES/advinhacao.py
+++ GAMES/advinhacao.py
@@ -2,24 +2,30 @@
 import requests
 import random
 
-# palavra = 'perfume'
-digitadas = []
-chances = 5
+#Gerar as palavras
 
-url = 'https://www.ime.usp.br/~pf/dicios/br-utf8.txt'
 
 resposta = requests.get(url)
-
 u = resposta.content.splitlines()
 
 u = [palavra.decode('utf-8') for palavra in u]
 
-palavras = random.sample(u, 1)
+
+
+sorte = random.randint(0, 261797)
+
+palavras_aleatorias = u[sorte]
+
+
+chances = 5
+digitadas = []
+
 
 while True:
-    # if chances <= 0:
-    #     print('suas chances acabaram, voce perdeu!')
-    #     break
+    if chances <= 0:
+        print('Você perdeu!!!')
+        print(f'A palavra era {palavras_aleatorias}.')
+        break
 
     letra = input('Digite uma letra: ')
 
@@ -29,31 +35,26 @@ while True:
 
     digitadas.append(letra)
 
-    if letra in palavras:
-        print(f'A letra {letra} existe na palavra secreta')
-
-    else:
-        print(f' A letra {letra} Nao existe na palavra secreta')
+    if letra not in palavras_aleatorias:
+        print(f'AFFFzzz: a letra "{letra}" NÃO EXISTE na palavra secreta.')
         digitadas.pop()
-
-    temp = ''
-
-    for letra_secreta in palavras:
+    else:
+        print(f'UHUULLL, a letra "{letra}" existe na palavra secreta.')
+    secreto_temporario = ''
+    for letra_secreta in palavras_aleatorias:
         if letra_secreta in digitadas:
-            temp += letra_secreta
-
+            secreto_temporario += letra_secreta
         else:
-            temp += '*'
+            secreto_temporario += '*'
 
-    if temp == palavras:
-        print(f'Que legal, Voce ganhou! A palavra é: {temp}')
+    if secreto_temporario == palavras_aleatorias:
+        print(f'Que legal, VOCÊ GANHOU!!! A palavra era {secreto_temporario}.')
         break
     else:
-        print(f'A palavra secreta esta assim: {temp}')
+        print(f'A palavra secreta está assim: {secreto_temporario}')
 
-    if letra not in palavras:
+    if letra not in palavras_aleatorias:
         chances -= 1
 
-    print(f'Voce ainda tem {chances} chances.')
+    print(f'Você ainda tem {chances} chances.')
 
-    print(palavras)
